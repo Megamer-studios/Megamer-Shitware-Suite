394266edbbd035d117ecea3fff0bc869c88dfd3a
Aquamarine Hoshino <cozoral@outlook.com>
2026-01-18 22:19:38 +0100

    chore: commit pending changes before .NET 10 upgrade

---
+++ ShittyExcel/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace ShittyExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            dataGridView1 = new DataGridView();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem = new ToolStripMenuItem();
+            loadToolStripMenuItem = new ToolStripMenuItem();
+            addToolStripMenuItem = new ToolStripMenuItem();
+            deleteToolStripMenuItem = new ToolStripMenuItem();
+            collumnToolStripMenuItem = new ToolStripMenuItem();
+            removeCellsToolStripMenuItem = new ToolStripMenuItem();
+            removeCollumnsToolStripMenuItem = new ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = Color.Cornsilk;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(0, 0);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(800, 450);
+            dataGridView1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.ImageScalingSize = new Size(20, 20);
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.BackColor = Color.Wheat;
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, addToolStripMenuItem, deleteToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 28);
+            menuStrip1.TabIndex = 2;
+            menuStrip1.Text = "menuStrip1";
+        
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { saveToolStripMenuItem, loadToolStripMenuItem });
+            fileToolStripMenuItem.Image = Properties.Resources.File;
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(66, 24);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToolStripMenuItem
+            // 
+            saveToolStripMenuItem.Image = Properties.Resources.Folder_Open;
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.Size = new Size(224, 26);
+            saveToolStripMenuItem.Text = "Save";
+            // 
+            // loadToolStripMenuItem
+            // 
+            loadToolStripMenuItem.Image = Properties.Resources.Folder_Closed;
+            loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            loadToolStripMenuItem.Size = new Size(224, 26);
+            loadToolStripMenuItem.Text = "Load";
+            // 
+            // addToolStripMenuItem
+            // 
+            addToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { collumnToolStripMenuItem });
+            addToolStripMenuItem.Image = Properties.Resources.Display;
+            addToolStripMenuItem.Name = "addToolStripMenuItem";
+            addToolStripMenuItem.Size = new Size(71, 24);
+            addToolStripMenuItem.Text = "Add";
+           
+            // 
+            // deleteToolStripMenuItem
+            // 
+            deleteToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { removeCellsToolStripMenuItem, removeCollumnsToolStripMenuItem });
+            deleteToolStripMenuItem.Image = Properties.Resources.My_Profile_Folder;
+            deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            deleteToolStripMenuItem.Size = new Size(87, 24);
+            deleteToolStripMenuItem.Text = "Delete";
+            // 
+            // collumnToolStripMenuItem
+            // 
+            collumnToolStripMenuItem.Image = Properties.Resources.Earth;
+            collumnToolStripMenuItem.Name = "collumnToolStripMenuItem";
+            collumnToolStripMenuItem.Size = new Size(224, 26);
+            collumnToolStripMenuItem.Text = "Collumn";
+            // 
+            // removeCellsToolStripMenuItem
+            // 
+            removeCellsToolStripMenuItem.Image = Properties.Resources.Manage_Your_Servers;
+            removeCellsToolStripMenuItem.Name = "removeCellsToolStripMenuItem";
+            removeCellsToolStripMenuItem.Size = new Size(209, 26);
+            removeCellsToolStripMenuItem.Text = "Remove cells";
+            // 
+            // removeCollumnsToolStripMenuItem
+            // 
+            removeCollumnsToolStripMenuItem.Image = Properties.Resources.Manage_Your_Servers;
+            removeCollumnsToolStripMenuItem.Name = "removeCollumnsToolStripMenuItem";
+            removeCollumnsToolStripMenuItem.Size = new Size(209, 26);
+            removeCollumnsToolStripMenuItem.Text = "Remove collumns";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            Controls.Add(dataGridView1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Shitty Excel";
+            FormClosed += Form1_FormClosed;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private ContextMenuStrip contextMenuStrip1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem loadToolStripMenuItem;
+        private ToolStripMenuItem addToolStripMenuItem;
+        private ToolStripMenuItem collumnToolStripMenuItem;
+        private ToolStripMenuItem deleteToolStripMenuItem;
+        private ToolStripMenuItem removeCellsToolStripMenuItem;
+        private ToolStripMenuItem removeCollumnsToolStripMenuItem;
+    }
+}
